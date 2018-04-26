5ac0f456afdc8ab7d2eaa991ed4ec09f40cb6a1e
Paul Galatic <pgalatic@gmail.com>
2018-04-26 17:56:59 -0400

    Successfully downloads top image from /r/wallpapers

--- backgrounder.py
+++ backgrounder.py
@@ -1,16 +1,77 @@
 # author: Paul Galatic github.com/pgalatic
 
+import oauth_info
+import requests
+import pickle
 import pprint
 import praw
+import os
+
+picture_path = 'C:\\Users\\Essence\\Documents\\Backgrounds' #TODO : Have user set this path
+file_path = 'C:\\Users\\Essence\\Documents\\Python Scripts\\backgrounder' #TODO : Make more general
+
+class Data(object):
+	def __init__(self, dict):
+		self.dict = dict
 
 def init():
-	reddit = praw.Reddit()
+	reddit = praw.Reddit(client_id=oauth_info.client_id,
+						 client_secret=oauth_info.client_secret,
+						 redirect_uri=oauth_info.redirect_uri,
+						 user_agent=oauth_info.user_agent)
+	reddit.read_only = True
+	return reddit
+
+def read_data():
+	dict = None
+	if (os.path.isfile('data.pkl')):
+		with open('data.pkl', 'rb') as input:
+			dict = pickle.load(input)
+	return dict
+	
+def write_data(dict):
+	with open('data.pkl', 'wb') as out:
+		pickle.dump(dict, out, pickle.HIGHEST_PROTOCOL)
+
+def save_image(picture_path, image_url):
+	with open(picture_path, 'wb') as handle:
+		response = requests.get(image_url, stream=True)
+		
+		if not response.ok:
+			print(response)
+		
+		for block in response.iter_content(1024):
+			if not block:
+				break
+			
+			handle.write(block)
+
+def main(reddit):
+	# if read_data is none
+	#	prompt user a la initial installation
+	# else
+	#	picture_path = dict.picture_path
+	#	image_id = dict.image_id + 1
+	#	dict.image_id = image_id
+	
+	r_wallpapers = reddit.subreddit('wallpapers')
+	
+	top_submission = None
+	# grabs top submission
+	for wallpaper in r_wallpapers.top(time_filter='day', limit=1):
+		top_submission = wallpaper
+	
+	permalink = top_submission.permalink
+	image_url = top_submission.url
+	
+	image_id = "/2.png"
+	
+	save_image(picture_path + image_id, image_url)
+	
+	
 	
 
-def main():
-	
-	r_wallpapers = praw.subreddit('wallpapers')
-	pprint.pprint(vars(r_wallpapers))
 
 reddit = init()
-main()
+
+main(reddit)
